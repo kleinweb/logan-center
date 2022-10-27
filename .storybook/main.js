b3c9acd6173c7b73db5077953370215aad0f18cf
Chris Montgomery <chris@cdom.io>
2022-10-26 22:13:02 -0400

    chore(sb): restore storybook

---
+++ .storybook/main.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-next"
+  ],
+  webpackFinal: async (config) => {
+    // Enable @ symbol aliases.
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../')
+    }
+
+    // Enable sourcemaps in Storybook.
+    config.module.rules.push({
+      test: /\.css$/i,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true
+          }
+        }
+      ]
+    })
+
+    return config
+  },
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
--- lib/hooks/useInternalLinkRouting.ts
+++ lib/hooks/useInternalLinkRouting.ts
@@ -3,14 +3,20 @@ import { useRouter } from 'next/router';
 import { getInternalLinkPathname } from '@/lib/links';
 
 /**
- * Using the Link component (next/link) for internal links instead of vanilla <a>
- * tags allows Next.js to load a page client-side and re-render with React
+ * Using the Link component (next/link) for internal links instead of vanilla
+ * <a> tags allows Next.js to load a page client-side and re-render with React
  * instead of making a full round-trip for another server-rendered page.
  *
- * However, sometimes <a> tags are embedded within markup that you need to render
- * directly with `dangerouslySetInnerHTML`. We can listen for clicks on these
- * links and, if they match our expectations for what constitues a relative link,
- * we can route them interally.
+ * However, sometimes <a> tags are embedded within markup that you need to
+ * render directly with `dangerouslySetInnerHTML`. We can listen for clicks on
+ * these links and, if they match our expectations for what constitues a
+ * relative link, we can route them interally.
+ *
+ * NOTE: If you run into issues with link routing in Storybook which may be
+ * caused by this hook, refer to the following resources for troubleshooting:
+ *
+ * - [the Storybook addon docs]{@link https://github.com/RyanClementsHax/storybook-addon-next#nextjs-routing}
+ *
  */
 export default function useLinkRouter() {
 	const router = useRouter();
