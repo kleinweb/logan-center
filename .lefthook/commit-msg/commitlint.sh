e4534a04b0d05b6d930087ea77120ef33561e0a8
Chris Montgomery <chris@cdom.io>
2022-10-31 16:18:09 -0400

    chore(reuse): specify some licenses in DEP-5 file

--- .lefthook/commit-msg/commitlint.sh
+++ .lefthook/commit-msg/commitlint.sh
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2022 Temple University
 #
-# SPDX-License-Identifier: CC0-1.0
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 # shellcheck disable=2005
 echo "$(head -n1 "$1")" | commitlint --color
--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,5 @@
-// SPDX-FileCopyrightText: 2021 Automattic
 // SPDX-FileCopyrightText: 2022 Temple University
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
-// SPDX-License-Identifier: MIT
 
 import '@testing-library/jest-dom/extend-expect'
