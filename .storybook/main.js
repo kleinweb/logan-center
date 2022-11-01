3e4ea210eec642890dbf7d9fa8ac84ef1d1fc9de
Chris Montgomery <chris@cdom.io>
2022-10-31 22:15:22 -0400

    fix(sb): add stories in components and pages dirs to search path

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,10 +5,7 @@
 const path = require('path')
 
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../@(components|pages|stories)/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
