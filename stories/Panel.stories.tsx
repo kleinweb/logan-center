4df883d0127f44e8ce705177232138bfd8eef5e7
Chris Montgomery <chris@cdom.io>
2022-11-02 18:55:09 -0400

    chore(reuse|stories): add headers + cleanup dead stories

--- stories/Panel.stories.tsx
+++ stories/Panel.stories.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 import { HeadingTwo } from '@/components/Headings'
 import { Button } from '@/components/Buttons'
 
