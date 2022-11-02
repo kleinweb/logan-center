a0e1080f3e3e6a29a41b92b6c2bb127b5b25977f
Chris Montgomery <chris@cdom.io>
2022-11-02 11:48:30 -0400

    refactor(stories|type): move headings to their own components

--- stories/Text.stories.tsx
+++ stories/Text.stories.tsx
@@ -1,36 +1,10 @@
-export const PageTitle = ({ children }) => {
-  return <h1 className="font-sans text-4xl font-bold">{children}</h1>
-}
-
-export const HeadingOne = ({ children }) => (
-  <h1 className="text-3xl font-bold">{children}</h1>
-)
-
-export const HeadingTwo = ({ children }) => (
-  <h2 className="text-2xl font-bold uppercase leading-normal tracking-wider">
-    {children}
-  </h2>
-)
-
-export const HeadingThree = ({ children }) => (
-  <h2 className="text-xl font-bold leading-relaxed tracking-widest">
-    {children}
-  </h2>
-)
-
-export const HeadingFour = ({ children }) => (
-  <h2 className="text-lg font-bold uppercase leading-loose tracking-widest">
-    {children}
-  </h2>
-)
-
-export const Dek = ({ children }) => <p className="text-lg">{children}</p>
-export const Paragraph = ({ children }) => (
-  <p className="text-base">{children}</p>
-)
-export const Supplemental = ({ children }) => (
-  <small className="text-sm italic">{children}</small>
-)
+import {
+  HeadingFour,
+  HeadingOne,
+  HeadingThree,
+  HeadingTwo,
+  PageTitle,
+} from '@/components/Headings'
 
 export const Inventory = () => {
   return (
@@ -40,9 +14,9 @@ export const Inventory = () => {
       <HeadingTwo>Heading 2</HeadingTwo>
       <HeadingThree>Heading 3</HeadingThree>
       <HeadingFour>Heading 4</HeadingFour>
-      <Dek>Blurb</Dek>
-      <Paragraph>Body Text</Paragraph>
-      <Supplemental>Byline</Supplemental>
+      <p className="text-lg">Blurb</p>
+      <p>Body Text</p>
+      <small className="text-sm italic">Byline</small>
     </>
   )
 }
