8a9f5ada8b17fc0426b35ab95b08712c7c9fc204
Chris Montgomery <chris@cdom.io>
2022-11-02 18:55:09 -0400

    chore(reuse|stories): add headers + cleanup dead stories

--- components/Headings/index.tsx
+++ components/Headings/index.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 export const classes = {
   heading: {
     h1: 'text-3xl font-bold',
--- stories/Buttons.stories.tsx
+++ stories/Buttons.stories.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 import type { Story } from '@ladle/react'
 import Button, { COLORS, Props, VARIANTS } from '@/components/Buttons'
 
