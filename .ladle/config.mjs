26dd2786754e0178d0138a4e70d75fe4f765f5f8
Chris Montgomery <chris@cdom.io>
2022-11-01 20:03:27 -0400

    feat(stories|deps): replace storybook with ladle

---
+++ .ladle/config.mjs
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2022 Temple University
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+export default {
+  stories: ['**/*.stories.{js,jsx,ts,tsx}', 'stories.custom.tsx'],
+}
