aa3737a80fffb9cf28dd8b524f2c1628142f8b40
Chris Montgomery <chris@cdom.io>
2022-11-03 14:50:35 -0400

    feat(icons|stories): provide each svg icon as a component

---
+++ components/Icons/SvgIcon.tsx
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { ReactNode } from 'react'
+
+const defaultAttrs: React.ComponentPropsWithoutRef<'svg'> = {
+  xmlns: 'http://www.w3.org/2000/svg',
+  width: 24,
+  height: 24,
+  fill: 'none',
+  stroke: 'currentColor',
+  strokeLinecap: 'round',
+  strokeLinejoin: 'round',
+  strokeWidth: 2,
+}
+
+export const SvgIcon = (props: { children: ReactNode }) => (
+  <svg {...defaultAttrs}>{props.children}</svg>
+)
---
+++ components/Icons/index.tsx
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import * as lucideIcons from './lucide'
+
+export const allIcons = { ...lucideIcons }
+
+export * from './lucide'
+export * from './SvgIcon'
