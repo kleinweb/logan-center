61b44658cf8a1620ce6d881fd072fdaa8a954a10
Chris Montgomery <chris@cdom.io>
2022-11-02 20:49:24 -0400

    fixup! feat: add functional button component and stories

--- stories/Buttons.stories.tsx
+++ stories/Buttons.stories.tsx
@@ -3,24 +3,19 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import type { Story } from '@ladle/react'
-import { Button, COLORS, Props } from '@/components/Buttons'
+import Button, { COLORS, Props, VARIANTS } from '@/components/Buttons'
 
 const defaultArgs = {
   children: 'Default',
 }
 
 const defaultArgTypes = {
+  variant: {
+    options: Object.values(VARIANTS),
+    control: { type: 'radio' },
+    defaultValue: VARIANTS.Solid,
+  },
   color: {
-    options: Object.values(COLORS),
-    control: { type: 'radio' },
-    defaultValue: COLORS.Slate,
-  },
-  fill: {
-    options: [...Object.values(COLORS), 'white'],
-    control: { type: 'radio' },
-    defaultValue: COLORS.Slate,
-  },
-  border: {
     options: Object.values(COLORS),
     control: { type: 'radio' },
     defaultValue: COLORS.Slate,
@@ -40,13 +35,10 @@ export const SparseButton = Template.bind({})
 SparseButton.args = {
   ...DefaultButton.args,
   children: 'Sparse Button',
-  fill: 'white',
+  variant: VARIANTS.Sparse,
 }
-SparseButton.argTypes = {
-  ...DefaultButton.argTypes,
-  border: { ...DefaultButton.argTypes.border, defaultValue: 'slate' },
-}
-delete SparseButton.argTypes.fill
+SparseButton.argTypes = DefaultButton.argTypes
+delete SparseButton.argTypes.variant
 
 const storyMeta = {
   title: 'Atoms/Buttons',
