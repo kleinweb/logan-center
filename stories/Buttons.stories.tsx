0799f4477c54ba27dde0d1d8dd24e2c68fd32ec0
Chris Montgomery <chris@cdom.io>
2022-11-04 17:02:02 -0400

    fix(stories): update story structure for storybook v7

--- stories/Buttons.stories.tsx
+++ stories/Buttons.stories.tsx
@@ -2,63 +2,38 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import type { Story } from '@ladle/react'
-import Button, { COLORS, Props, VARIANTS } from '@/components/Buttons'
+import Button, { VARIANTS } from '@/components/Buttons'
 import { ChevronRight, Icon } from '@/components/Icons'
 
-const defaultArgs = {
-  children: 'Default',
-}
-
-const defaultArgTypes = {
-  variant: {
-    options: Object.values(VARIANTS),
-    control: { type: 'radio' },
-    defaultValue: VARIANTS.Solid,
-  },
-  color: {
-    options: Object.values(COLORS),
-    control: { type: 'radio' },
-    defaultValue: COLORS.Slate,
-  },
-}
-
-const Template: Story<Props> = (args) => <Button {...args} />
-
-export const DefaultButton = Template.bind({})
-DefaultButton.args = {
-  ...defaultArgs,
-  children: 'Button Dafoe',
-}
-DefaultButton.argTypes = defaultArgTypes
-
-export const ButtonWithIcon = Template.bind({})
-ButtonWithIcon.args = {
-  ...defaultArgs,
-  children: (
-    <>
-      Button Dafoe{' '}
-      <div className="-mr-2 mt-[-2px]">
-        <Icon label="Read More">
-          <ChevronRight />
-        </Icon>
-      </div>
-    </>
-  ),
-}
-ButtonWithIcon.argTypes = defaultArgTypes
-
-export const SparseButton = Template.bind({})
-SparseButton.args = {
-  ...DefaultButton.args,
-  children: 'Sparse Button',
-  variant: VARIANTS.Sparse,
-}
-SparseButton.argTypes = DefaultButton.argTypes
-delete SparseButton.argTypes.variant
-
-const storyMeta = {
+export default {
   title: 'Atoms/Buttons',
+  component: Button,
 }
 
-export default storyMeta
+export const DefaultButton = {
+  args: {
+    children: 'Button Text',
+  },
+}
+
+export const ButtonWithIcon = {
+  args: {
+    children: (
+      <>
+        Button Dafoe{' '}
+        <div className="-mr-2 mt-[-2px]">
+          <Icon label="Read More">
+            <ChevronRight />
+          </Icon>
+        </div>
+      </>
+    ),
+  },
+}
+
+export const SparseButton = {
+  args: {
+    children: 'Sparse Button',
+    variant: VARIANTS.Sparse,
+  },
+}
