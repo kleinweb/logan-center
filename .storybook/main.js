21f72a224ca3f99b1a7e17b7a051bb4525c417a6
Chris Montgomery <chris@cdom.io>
2022-11-04 15:18:34 -0400

    feat(stories): storybook 7 alpha

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+module.exports = {
+  stories: ['../@(components|pages|stories)/**/*.stories.@(js|ts|jsx|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+}
