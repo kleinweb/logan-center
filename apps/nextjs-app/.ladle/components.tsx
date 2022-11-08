1e3d1902c9e8f12c152137002cd5fae4c70d3b36
Chris Montgomery <chris@cdom.io>
2022-11-08 08:34:22 -0500

    chore: remove all component library tools to simplify codebase

--- apps/nextjs-app/.ladle/components.tsx
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Temple University
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-import '../styles/index.css'
--- apps/nextjs-app/.ladle/config.mjs
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Temple University
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-export default {
-  stories: ['**/*.stories.{js,jsx,ts,tsx}', 'stories.custom.tsx'],
-}
