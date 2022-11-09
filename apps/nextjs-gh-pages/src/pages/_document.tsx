8d40062476e401800064ba19afcdea08dfea8670
Chris Montgomery <chris@cdom.io>
2022-11-09 09:37:31 -0500

    fix(env:ghp): convert `Document` to a function component

--- apps/nextjs-gh-pages/src/pages/_document.tsx
+++ apps/nextjs-gh-pages/src/pages/_document.tsx
@@ -1,34 +1,18 @@
-import NextDocument, {
-  DocumentContext,
-  DocumentInitialProps,
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from 'next/document'
+// SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
+// SPDX-License-Identifier: GPL-3.0-or-later
 
-class Document extends NextDocument {
-  static async getInitialProps(
-    ctx: DocumentContext,
-  ): Promise<DocumentInitialProps> {
-    const initialProps = await NextDocument.getInitialProps(ctx)
+import { Html, Head, Main, NextScript } from 'next/document'
 
-    return initialProps
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="stylesheet" href="https://use.typekit.net/cwz8dss.css" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="stylesheet" href="https://use.typekit.net/cwz8dss.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default Document
