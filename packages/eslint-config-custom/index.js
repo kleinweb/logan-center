49f51655e801abcc2083d62b3b76e68f72f7a046
Chris Montgomery <chris@cdom.io>
2022-11-09 21:00:29 -0500

    fix(eslint): globally disable `no-undef` due to ts interface incompat

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -9,5 +9,9 @@ module.exports = {
   plugins: ['tailwindcss'],
   rules: {
     'no-console': ['error', { allow: ['warn', 'error'] }],
+    // Disabled due to false positives with TypeScript interfaces. `tsc` will
+    // catch undefined variables instead.
+    // https://github.com/typescript-eslint/typescript-eslint/issues/131
+    'no-undef': 0,
   },
 }
