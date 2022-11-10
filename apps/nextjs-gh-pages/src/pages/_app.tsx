c06ef9313e436b31d07d380322a4f35b09a6ac2b
Chris Montgomery <chris@cdom.io>
2022-11-09 20:17:40 -0500

    feat(ghp|telemetry): add google analytics

--- apps/nextjs-gh-pages/src/pages/_app.tsx
+++ apps/nextjs-gh-pages/src/pages/_app.tsx
@@ -1,8 +1,18 @@
+// SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 import '../styles/globals.css'
 import 'ui/styles.css'
 
 import type { AppProps } from 'next/app'
 
+import Telemetry from '@/components/Telemetry'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Telemetry />
+      <Component {...pageProps} />
+    </>
+  )
 }
