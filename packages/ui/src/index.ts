d5ddcfe1193a253ffe84457484e25cc1c48d5101
Chris Montgomery <chris@cdom.io>
2022-11-10 17:27:31 -0500

    refactor: make headings module/components more useful

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,8 +3,16 @@
 
 // FIXME: these exports all exist in the global namespace of this package
 export * from './components/Buttons'
-export * from './components/Headings'
 export * from './components/Icons'
 
 export { ActiveLink } from './components/ActiveLink'
+export {
+  HeadingOne,
+  HeadingTwo,
+  HeadingThree,
+  HeadingFour,
+  HeadingFive,
+  HEADING_CLASSES,
+} from './components/Headings'
+
 export { ImageObject } from './components/Image'
--- packages/ui/src/stories/Text.stories.tsx
+++ packages/ui/src/stories/Text.stories.tsx
@@ -3,20 +3,20 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import {
-  HeadingFour,
   HeadingOne,
-  HeadingThree,
   HeadingTwo,
-  PageTitle,
+  HeadingThree,
+  HeadingFour,
+  HeadingFive,
 } from '@/components/Headings'
 
 export const Text = () => (
   <>
-    <PageTitle>Page Title</PageTitle>
-    <HeadingOne>Heading 1</HeadingOne>
+    <HeadingOne>Page Title</HeadingOne>
     <HeadingTwo>Heading 2</HeadingTwo>
     <HeadingThree>Heading 3</HeadingThree>
     <HeadingFour>Heading 4</HeadingFour>
+    <HeadingFive>Heading 5</HeadingFive>
     <p className="text-lg">Blurb</p>
     <p>Body Text</p>
     <small className="text-sm italic">Byline</small>
