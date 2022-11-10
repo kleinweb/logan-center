e27765debd712f6336fbdc2f322ae30cb7839029
Chris Montgomery <chris@cdom.io>
2022-11-10 17:31:16 -0500

    refactor(icons): clean up exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 // FIXME: these exports all exist in the global namespace of this package
-export * from './components/Icons'
 
 export { ActiveLink } from './components/ActiveLink'
+
 export { Button, BUTTON_COLORS, BUTTON_VARIANTS } from './components/Buttons'
 
 export {
@@ -15,5 +15,7 @@ export {
   HeadingFive,
   HEADING_CLASSES,
 } from './components/Headings'
+
+export { Icon } from './components/Icons'
 
 export { ImageObject } from './components/Image'
