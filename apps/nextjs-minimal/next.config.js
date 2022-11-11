00d0f8891076ebd1e1d832449e7d801cb0b03447
Chris Montgomery <chris@cdom.io>
2022-11-11 01:55:59 -0500

    fix: mark `ui` package to be transpiled

--- apps/nextjs-minimal/next.config.js
+++ apps/nextjs-minimal/next.config.js
@@ -7,6 +7,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    transpilePackages: ['ui'],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
