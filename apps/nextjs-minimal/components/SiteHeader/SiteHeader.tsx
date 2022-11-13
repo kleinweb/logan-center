c3c2b7daa3de6c92273ac6f34f7773bfb79491b9
Chris Montgomery <chris@cdom.io>
2022-11-12 19:38:58 -0500

    fix(banner): use inline logo with fine-tuned links
    feat(banner): replace "Home" link with external "About" link

--- apps/nextjs-minimal/components/SiteHeader/SiteHeader.tsx
+++ apps/nextjs-minimal/components/SiteHeader/SiteHeader.tsx
@@ -1,34 +1,69 @@
 // SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import Link from 'next/link'
+import clsx from 'clsx'
+import Link, { LinkProps } from 'next/link'
+import { __ } from '@wordpress/i18n'
+import { ExternalLink } from 'lucide-react'
 
-import LogoFull from 'shared-assets/logos/logo--full--duo.svg'
-import LogoMinimal from 'shared-assets/logos/logo--gfx--duo.svg'
+import { ActiveLink as ActiveLinkComponent } from 'ui'
+import { ActiveLinkProps as ActiveLinkComponentProps } from 'ui'
+
 import Container from '../Container'
-import { ActiveLink } from 'ui'
 
-const navItems = [
-  ['/', 'Home'],
-  ['/podcast', 'Podcast'],
-]
+import LogoMinimal from 'shared-assets/logos/logo--gfx--duo.svg'
+import LogoText from 'shared-assets/logos/logo--text.svg'
+
+type ActiveLinkProps = {
+  href: LinkProps['href']
+  children: ActiveLinkComponentProps['children']
+}
+
+const LogoInline = () => (
+  <Link href="/" className="flex gap-1">
+    <LogoMinimal />
+    <LogoText />
+  </Link>
+)
+
+const NavItem = (props: ActiveLinkProps) => (
+  <li className="text-xs first:ml-2 last:-mr-2">
+    <ActiveLinkComponent
+      href={props.href}
+      activeClassName="text-teal-500 font-bold"
+    >
+      {props.children}
+    </ActiveLinkComponent>
+  </li>
+)
+
+const navLinkStyles = 'p-2 uppercase'
 
 export default function SiteHeader() {
   return (
     <Container>
-      <div className="flex align-middle">
-        <Link href="/" className="block grow">
-          <LogoMinimal className="block min-[468px]:hidden" />
-          <LogoFull className="hidden max-h-16 min-[468px]:block" />
-        </Link>
-        <nav className="flex items-center">
-          {navItems.map(([path, label]) => (
-            <li key={label} className="list-none">
-              <ActiveLink href={path} activeClassName="text-teal-500 font-bold">
-                <a className="px-2 uppercase first:ml-2 last:-mr-2">{label}</a>
-              </ActiveLink>
-            </li>
-          ))}
+      <div className="flex items-center justify-between">
+        <div className="grow">
+          <div className="max-w-sm">
+            <LogoInline />
+          </div>
+        </div>
+        <nav>
+          <ul className="flex items-center justify-end">
+            <NavItem href="/podcast">
+              <a className={navLinkStyles}>{__('Podcast')}</a>
+            </NavItem>
+            <NavItem href="https://klein.temple.edu/news/2021/07/klein-college-establishes-logan-center-urban-investigative-reporting">
+              <a
+                rel="noreferrer external"
+                target="_blank"
+                className={clsx('inline-flex items-center', navLinkStyles)}
+              >
+                {__('About', 'logan-center')}
+                <ExternalLink className="h-4" />
+              </a>
+            </NavItem>
+          </ul>
         </nav>
       </div>
     </Container>
