5394bb519aa5a76efd46c3333ed4278beee739d8
Chris Montgomery <chris@cdom.io>
2022-11-13 01:44:13 -0500

    fix(siteFooter): adjust social icons to spec

--- apps/nextjs-app/components/SiteFooter/SiteFooter.tsx
+++ apps/nextjs-app/components/SiteFooter/SiteFooter.tsx
@@ -31,7 +31,7 @@ export default function SiteFooter() {
           <a
             href="https://twitter.com/TULoganCenter"
             target="_blank"
-            rel="noreferrer"
+            rel="noreferrer external"
           >
             <Twitter />
           </a>
@@ -40,7 +40,7 @@ export default function SiteFooter() {
           <a
             href="https://www.facebook.com/tulogancenter/"
             target="_blank"
-            rel="noreferrer"
+            rel="noreferrer external"
           >
             <Facebook />
           </a>
@@ -49,7 +49,7 @@ export default function SiteFooter() {
           <a
             href="https://www.facebook.com/tulogancenter/"
             target="_blank"
-            rel="noreferrer"
+            rel="noreferrer external"
           >
             <Instagram />
           </a>
