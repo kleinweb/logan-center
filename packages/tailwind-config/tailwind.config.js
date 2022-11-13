b0d28b722f1bfb9b297dddf8bc159b760782584b
Chris Montgomery <chris@cdom.io>
2022-11-13 16:23:13 -0500

    fix(theme): override tailwind breakpoints to remove `2xl` size

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -16,6 +16,12 @@ const secondaryPalette = {
 module.exports = {
   content: [`./**/*.{js,ts,jsx,tsx,mdx}`],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     colors: {
       primary: primaryPalette,
       secondary: secondaryPalette,
