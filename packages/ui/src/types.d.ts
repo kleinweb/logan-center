7b150b59c2d214b8c748a6657822118e891ffd91
Chris Montgomery <chris@cdom.io>
2022-11-13 23:07:47 -0500

    feat: add custom type definitions file in ui lib

---
+++ packages/ui/src/types.d.ts
@@ -0,0 +1,8 @@
+import { ChevronRight } from 'lucide-react'
+
+/**
+ * Icon component of the type exported by the `lucide-react` library.
+ *
+ * Note that the use of {@link ChevronRight} here is arbitrary.
+ */
+export type LucideIconComponent = typeof ChevronRight
