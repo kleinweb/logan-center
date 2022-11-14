d8b0077de571297de846050e787535732cd715d0
Chris Montgomery <chris@cdom.io>
2022-11-13 23:39:40 -0500

    fix(home): content updates for launch

--- apps/nextjs-minimal/pages/index.tsx
+++ apps/nextjs-minimal/pages/index.tsx
@@ -4,8 +4,9 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import { __ } from '@wordpress/i18n'
+import { ExternalLink } from 'lucide-react'
 
-import { ImageObject, LinkButton } from 'ui'
+import { Button, ButtonWithIcon, ImageObject, LinkButton } from 'ui'
 
 import { SITE_NAME } from '@/lib/constants'
 import Layout from '@/components/Layout/Layout'
@@ -18,8 +19,6 @@ import podcastImage from 'shared-assets/images/podcast--poster--art_only.jpg'
 import youGotThisImage from 'shared-assets/images/photos/mastermanschool-firstday01-crop-1024x576.jpeg'
 
 const pageTitle = `Home | ${SITE_NAME}`
-
-const heroDoc = `Klein College’s Logan Center for Urban Investigative Reporting focuses on the issues facing Philadelphia and other large American cities, including gun violence, economic inequality, education and health disparities, crumbling infrastructure and eroding trust in institutions.`
 const aboutLoganDoc = `Klein College of Media and Communication launched the Jonathan Logan Family Foundation Center for Urban Investigative Reporting in the summer of 2021 thanks to a $1.2 million founding grant from the Jonathan Logan Family Foundation of Berkeley, California. The Center focuses exclusively on the issues facing Philadelphia and other large American cities such as gun violence, economic inequality, education and health disparities, crumbling infrastructure and eroding trust in institutions. Through the Logan Center, Klein students and faculty report not only on these problems, but on potential solutions, closely examining what has worked well in other cities across the nation and the globe.`
 
 export default function Home() {
@@ -33,102 +32,134 @@ export default function Home() {
 
       <ImageObject src={heroImage} alt="" aria-hidden />
 
-      <div className="bg-slate-100 py-6">
+      <div className="mb-6 bg-slate-100 py-6">
         <Container>
-          <h2 className="text-primary-dark mb-5 text-2xl font-bold leading-tight">
-            {__(
-              'Investigating the issues affecting urban communities',
-              'logan-center',
-            )}
-          </h2>
-          <p className="text-base">{__(heroDoc, 'logan-center')}</p>
+          <div className="mb-6 space-y-5">
+            <h2 className="text-primary-dark text-xl font-bold leading-tight md:text-2xl">
+              {__(
+                'Investigating the issues affecting urban communities',
+                'logan-center',
+              )}
+            </h2>
+            <p className="text-base">
+              {__(
+                `Klein College’s Logan Center for Urban Investigative Reporting focuses on the issues facing Philadelphia and other large American cities, including gun violence, economic inequality, education and health disparities, crumbling infrastructure and eroding trust in institutions.`,
+                'logan-center',
+              )}
+            </p>
+          </div>
         </Container>
       </div>
 
-      <section className="py-6">
+      <section className="mb-6 py-6">
         <Container>
-          <div className="space-y-5 space-y-reverse">
-            <h2 className="text-primary-dark mb-5 text-right text-2xl font-bold uppercase leading-snug tracking-wider">
-              {__('Listen to our new podcast', 'logan-center')}
-            </h2>
-            <div className="mb-5">
-              <p className="text-right text-lg">
-                {__(
-                  'The Logan Center has teamed up with WHYY to discuss Stop & Frisk in Philadelphia',
-                  'logan-center',
-                )}
-              </p>
+          <div className="flex flex-col">
+            <div className="order-2 mb-3 flex flex-col">
+              <h2 className="text-primary-dark order-2 mb-2 text-right text-xl font-bold uppercase leading-tight tracking-wider">
+                {__('Stop and Frisk: Revisit or Resist', 'logan-center')}
+              </h2>
+              <h3 className="order-1 mb-1 text-right text-base font-bold uppercase tracking-wider text-red-600">
+                {__('Listen to our new podcast', 'logan-center')}
+              </h3>
             </div>
-            <div className="text-right">
-              <LinkButton href="/podcast" color="red">
-                Learn More
-              </LinkButton>
+
+            <div className="order-3 flex flex-col space-y-4">
+              <div className="">
+                <p className="text-right text-base">
+                  {__(
+                    'Temple University’s Logan Center has teamed up with WHYY News to discuss the possibility of Stop and Frisk returning to Philadelphia in a new five-episode podcast.',
+                    'logan-center',
+                  )}
+                </p>
+              </div>
+
+              <div className="text-right">
+                <Link
+                  href="https://whyy.org/programs/stop-and-frisk-revisit-or-resist/"
+                  rel="noreferrer external"
+                  target="_blank"
+                >
+                  <Button>
+                    Listen Now on WHYY
+                    <ExternalLink className="relative bottom-[0.5px] -mr-1 py-1 pl-1" />
+                  </Button>
+                </Link>
+              </div>
             </div>
-            <Link href="/podcast">
-              <ImageObject
-                src={podcastImage}
-                alt={__('Promotional poster for the podcast.')}
-                aspect="banner"
-                className="object-top"
-              />
-            </Link>
+
+            <div className="order-1 mb-6">
+              <Link href="/podcast">
+                <ImageObject
+                  src={podcastImage}
+                  alt={__('Promotional poster for the podcast.')}
+                  aspect="banner"
+                  className="object-top"
+                />
+              </Link>
+            </div>
           </div>
         </Container>
       </section>
 
-      <section className="bg-primary-light mt-20 pt-6 pb-12">
+      <section className="bg-primary-light mt-20 py-6 pb-20">
         <Container>
           <div className="text-primary-accent relative -mt-24 flex flex-row-reverse py-8">
             <BarsMotif />
           </div>
-          <div className="space-y-5 space-y-reverse">
-            <h2 className="mb-5 text-2xl font-bold uppercase leading-snug tracking-wider text-red-600">
+
+          <div className="space-y-5">
+            <h2 className="text-xl font-bold uppercase tracking-wider text-red-600">
               About the Logan Center
             </h2>
 
-            <div className="mb-5">
-              <p className="text-base">{__(aboutLoganDoc, 'logan-center')}</p>
-            </div>
+            <p className="text-base">{__(aboutLoganDoc, 'logan-center')}</p>
 
             <div className="text-right">
-              <LinkButton
-                href="https://klein.temple.edu"
-                color="red"
-                variant="sparse"
-              >
-                Learn More
-              </LinkButton>
+              <Link className="block" href="mailto:tulogancenter@temple.edu">
+                <ButtonWithIcon variant="sparse">Email Us</ButtonWithIcon>
+              </Link>
             </div>
           </div>
         </Container>
       </section>
 
-      <section className="py-6">
+      <section className="pt-6 pb-16">
         <Container>
           <div className="text-primary-accent relative -mt-24 flex py-8">
             <BarsMotif />
           </div>
-          <div className="space-y-5 space-y-reverse">
-            <h2 className="text-primary-dark mb-5 text-2xl font-bold uppercase leading-snug tracking-wider">
-              Education Disparities Project
-            </h2>
-
-            <div className="mb-5">
-              <p className="text-base">
-                {__(
-                  `Temple's Logan Center for Urban Investigative Reporting and Billy Penn at WHYY are partnering on a yearlong reporting project examining disparities within the Philly School District, with the goal of identifying solutions that can lift up underserved schools and increase equity in public education.`,
-                  'logan-center',
-                )}
-              </p>
+          <div className="flex flex-col">
+            <div className="order-2 mb-3 flex flex-col">
+              <h2 className="text-primary-dark order-2 mb-2 text-right text-xl font-bold uppercase leading-tight tracking-wider">
+                {__('Education Disparities Project', 'logan-center')}
+              </h2>
+              <h3 className="order-1 mb-1 text-right text-base font-bold uppercase tracking-wider text-red-600">
+                {__('Read our latest stories', 'logan-center')}
+              </h3>
             </div>
 
-            <div className="text-right">
-              <LinkButton
-                href="https://billypenn.com/stories/education-disparities-in-philadelphia/"
-                color="red"
-              >
-                Read More
-              </LinkButton>
+            <div className="order-3 flex flex-col space-y-4">
+              <div className="">
+                <p className="text-right text-base">
+                  {__(
+                    `Temple’s Logan Center for Urban Investigative Reporting and Billy Penn at WHYY are partnering on a yearlong reporting project examining disparities within the Philly School District, with the goal of identifying solutions that can lift up underserved schools and increase equity in public education.`,
+                    'logan-center',
+                  )}
+                </p>
+              </div>
+
+              <div className="text-right">
+                <Link
+                  href="https://billypenn.com/stories/education-disparities-in-philadelphia/"
+                  rel="noreferrer external"
+                  target="_blank"
+                >
+                  <Button>
+                    Read more
+                    <ExternalLink className="relative bottom-[0.5px] -mr-1 py-1 pl-1" />
+                  </Button>
+                </Link>
+              </div>
             </div>
           </div>
         </Container>
