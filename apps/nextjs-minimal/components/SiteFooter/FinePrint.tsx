7b65d88b169dbacb1f89a3ca8e9db460c4cc4d52
Chris Montgomery <chris@cdom.io>
2022-11-14 09:45:15 -0500

    feat(siteFooter|legal): add link to university privacy statement

--- apps/nextjs-minimal/components/SiteFooter/FinePrint.tsx
+++ apps/nextjs-minimal/components/SiteFooter/FinePrint.tsx
@@ -1,10 +1,27 @@
 const currentYear = new Date().getFullYear()
-const copyrightText = `
-  Copyright © ${currentYear} Temple University. All rights reserved.
-`
 
 export default function FinePrint() {
   return (
-    <small className="block text-center text-slate-300">{copyrightText}</small>
+    <div className="space-y-1 text-center text-slate-300">
+      <nav>
+        <ul className="inline-flex list-none">
+          <li className="">
+            <small>
+              <a
+                href="https://www.temple.edu/privacy-statement/"
+                rel="noreferrer external"
+                target="_blank"
+                className="text-red-300 after:content-['_↗'] hover:underline"
+              >
+                Privacy Statement
+              </a>
+            </small>
+          </li>
+        </ul>
+      </nav>
+      <small className="block">
+        {`Copyright © ${currentYear} Temple University. All rights reserved.`}
+      </small>
+    </div>
   )
 }
