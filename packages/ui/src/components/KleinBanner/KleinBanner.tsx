46eb493332311c9f2a3406544e4ef00ce3f10a70
Chris Montgomery <chris@cdom.io>
2022-12-05 11:06:41 -0500

    feat(ui): add component for klein recirc banner

---
+++ packages/ui/src/components/KleinBanner/KleinBanner.tsx
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2022 Temple University
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import { __ } from '@wordpress/i18n'
+
+import TempleLogo from 'temple-assets/logos/temple--logo--icon.svg'
+
+export default function KleinBanner() {
+  return (
+    <div className="bg-neutral-800 text-white">
+      <div className="container relative mx-auto flex items-center justify-end px-5">
+        <a
+          href="https://klein.temple.edu"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center gap-4 uppercase tracking-wide"
+        >
+          <p className="grow font-bold">{__('Explore')} klein.temple.edu</p>
+          <div className="h-[40px] w-[40px] bg-white">
+            <TempleLogoIcon />
+          </div>
+        </a>
+      </div>
+    </div>
+  )
+}
