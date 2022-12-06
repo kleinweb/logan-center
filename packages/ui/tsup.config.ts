faccbc49d9d127a59711861f38cbf69000063c77
Chris Montgomery <chris@cdom.io>
2022-12-05 22:14:59 -0500

    feat(temple-assets): package svgs with rollup and svgr

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'tsup'
-import SVGRPlugin from 'esbuild-plugin-svgr'
 
 export default defineConfig({
   entry: ['src/index.ts'],
   sourcemap: true,
   external: ['react'],
-  esbuildPlugins: [SVGRPlugin()],
 })
