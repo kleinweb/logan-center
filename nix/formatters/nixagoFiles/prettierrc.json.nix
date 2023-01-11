523178c210d8d557ca6c38eec41025f18239986b
Chris Montgomery <chris@cdom.io>
2023-01-11 18:13:53 -0500

    fix: no rewrap markdown

--- nix/formatters/nixagoFiles/prettierrc.json.nix
+++ nix/formatters/nixagoFiles/prettierrc.json.nix
@@ -1,11 +1,9 @@
 # SPDX-FileCopyrightText: 2022 Temple University
-#
 # SPDX-License-Identifier: GPL-3.0-or-later
 {
   flake.nixagoFiles."prettierrc" = {
     output = ".prettierrc.json";
     configData = {
-      proseWrap = "always";
       semi = false;
       singleQuote = true;
       trailingComma = "all";
