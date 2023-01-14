282aef8bbb72184a501c0f331f9498623a8346d2
Chris Montgomery <chris@cdom.io>
2023-01-14 00:25:49 -0500

    chore(deps): bump

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -9,6 +9,7 @@ module.exports = {
   plugins: ['tailwindcss'],
   rules: {
     'no-console': ['error', { allow: ['warn', 'error'] }],
+    // TODO(2022-01-30): reconsider -- the issue is closed, and the one error i ran into seems legit
     // Disabled due to false positives with TypeScript interfaces. `tsc` will
     // catch undefined variables instead.
     // https://github.com/typescript-eslint/typescript-eslint/issues/131
