ccc0960121999ab952baf0b9036d242f2e531c25
Chris Montgomery <chris@cdom.io>
2023-01-20 11:23:00 -0500

    chore(reuse): update copyright year

--- apps/nextjs-minimal/components/Post/PostTitle.tsx
+++ apps/nextjs-minimal/components/Post/PostTitle.tsx
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
+// SPDX-FileCopyrightText: 2022-2023 Temple University <kleinweb@temple.edu>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import { ReactNode } from 'react'
@@ -11,16 +11,16 @@ const PostTitle = ({ children }: Props) => {
   return (
     <h1
       className={`
-        text-2xl
-        md:text-3xl
-        lg:text-4xl
-        font-bold
-        tracking-tighter
-        leading-tight
-        md:leading-none
         mb-12
         text-center
+        text-2xl
+        font-bold
+        leading-tight
+        tracking-tighter
         md:text-left
+        md:text-3xl
+        md:leading-none
+        lg:text-4xl
       `}
     >
       {children}
--- apps/nextjs-minimal/components/SiteFooter/SiteFooter.tsx
+++ apps/nextjs-minimal/components/SiteFooter/SiteFooter.tsx
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 Temple University <kleinweb@temple.edu>
+// SPDX-FileCopyrightText: 2022-2023 Temple University <kleinweb@temple.edu>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import Container from '../Container'
