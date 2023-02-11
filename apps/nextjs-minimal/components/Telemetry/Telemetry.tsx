6b89ca94f919eb77c1a5b7980bb325f9384dc4cd
Chris Montgomery <chris@cdom.io>
2023-02-11 01:19:57 -0500

    chore(reuse): yep

--- apps/nextjs-minimal/components/Telemetry/Telemetry.tsx
+++ apps/nextjs-minimal/components/Telemetry/Telemetry.tsx
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 Temple University <kleinweb@temple.edu>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 import { useRouter } from 'next/router'
 import Script from 'next/script'
 import { useEffect } from 'react'
--- apps/nextjs-minimal/lib/constants.ts
+++ apps/nextjs-minimal/lib/constants.ts
@@ -1,1 +1,4 @@
+// SPDX-FileCopyrightText: 2023 Temple University <kleinweb@temple.edu>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 export const SITE_NAME = 'Logan Center for Urban Investigative Reporting'
