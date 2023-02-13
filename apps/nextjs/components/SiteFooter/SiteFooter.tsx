09d445e15929bdeca6e5d3f92707923ed8a16558
Chris Montgomery <chris@cdom.io>
2023-02-13 11:49:47 -0500

    refactor: migrate components from `nextjs-minimal`

--- apps/nextjs/components/SiteFooter/SiteFooter.tsx
+++ apps/nextjs/components/SiteFooter/SiteFooter.tsx
@@ -1,60 +1,43 @@
-// SPDX-FileCopyrightText: 2021 Automattic
 // SPDX-FileCopyrightText: 2022-2023 Temple University <kleinweb@temple.edu>
-//
-// SPDX-License-Identifier: GPL-3.0-or-later OR MIT
+// SPDX-License-Identifier: GPL-3.0-or-later
 
-import { Twitter, Facebook, Instagram } from 'lucide-react'
+import Container from '../Container'
+import SocialIcons from './SocialIcons'
 
+import { TempleLogo } from '@kleinweb/tu-assets'
+import LogoFull from '@kleinweb/logan-center__site-assets/logos/logo--full--duo.svg'
 import { Icon } from '@kleinweb/logan-center__ui'
+import Link from 'next/link'
+import FinePrint from './FinePrint'
 
-/**
- * Server-side data loading must take place in pages. If you need to load data
- * to render this component, you must pass it down as props (or load it client-
- * side.)
- */
 export default function SiteFooter() {
   return (
-    <>
-      <div className="mb-8">
-        <address className="not-italic leading-snug">
-          <p className="mb-1 font-bold leading-tight">
-            Klein College of Media and Communication
-          </p>
-          <p>Annenberg Hall</p>
-          <p>2020 N. 13th St.</p>
-          <p>Philadelphia, PA 19122</p>
+    <Container>
+      <div className="mx-auto mb-10 flex flex-wrap items-start justify-center sm:flex-nowrap sm:justify-between sm:gap-8">
+        <address className="mb-10 max-w-[300px] not-italic leading-snug ">
+          <a
+            href="https://klein.temple.edu"
+            target="_blank"
+            rel="noreferrer external"
+            className="block"
+          >
+            <Icon label="Temple University">
+              <TempleLogo />
+            </Icon>
+            <p className="mb-1 text-base font-bold leading-tight">
+              Klein College of Media and Communication
+            </p>
+          </a>
         </address>
+        <Link href="/" className="block max-w-[300px]">
+          <LogoFull className="block w-full" />
+        </Link>
       </div>
-      <hr className="my-5 mb-8 block border-neutral-400" />
-      <div className="flex justify-center">
-        <Icon label="Follow us on Twitter">
-          <a
-            href="https://twitter.com/TULoganCenter"
-            target="_blank"
-            rel="noreferrer external"
-          >
-            <Twitter />
-          </a>
-        </Icon>
-        <Icon label="Follow us on Facebook">
-          <a
-            href="https://www.facebook.com/tulogancenter/"
-            target="_blank"
-            rel="noreferrer external"
-          >
-            <Facebook />
-          </a>
-        </Icon>
-        <Icon label="Follow us on Instagram">
-          <a
-            href="https://www.facebook.com/tulogancenter/"
-            target="_blank"
-            rel="noreferrer external"
-          >
-            <Instagram />
-          </a>
-        </Icon>
+      <hr className="my-5 border-white/10" />
+      <div className="mb-5 flex justify-center">
+        <SocialIcons />
       </div>
-    </>
+      <FinePrint />
+    </Container>
   )
 }
