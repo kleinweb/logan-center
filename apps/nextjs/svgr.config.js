ddf1cfebd3528efd17a15d3f75f8b8fa4847bbf8
Chris Montgomery <chris@cdom.io>
2023-02-13 15:40:24 -0500

    fix(assets): disable typescript output for svgr

--- apps/nextjs/svgr.config.js
+++ apps/nextjs/svgr.config.js
@@ -1,6 +1,4 @@
 // SPDX-FileCopyrightText: 2023 Temple University <kleinweb@temple.edu>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-module.exports = {
-  typescript: true,
-}
+module.exports = {}
