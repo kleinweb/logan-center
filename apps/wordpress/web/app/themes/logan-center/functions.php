cd39daddb5b23315c831d00a16cea807c294cd65
Chris Montgomery <chris@cdom.io>
2023-02-13 22:44:21 -0500

    fix(wp:theme): assorted wp-admin clutter cleanup

--- apps/wordpress/web/app/themes/logan-center/functions.php
+++ apps/wordpress/web/app/themes/logan-center/functions.php
@@ -7,3 +7,50 @@
  * @package logan-center-theme
  * @since 1.0.0
  */
+
+ // Register theme defaults.
+add_action('after_setup_theme', function () {
+    add_theme_support('post-thumbnails');
+
+    register_nav_menus([
+        'navigation' => __('Navigation'),
+    ]);
+});
+
+// Remove admin menu items.
+add_action('admin_init', function () {
+    remove_menu_page('edit-comments.php'); // Comments
+    // remove_menu_page('edit.php?post_type=page'); // Pages
+    // remove_menu_page('edit.php'); // Posts
+    remove_menu_page('index.php'); // Dashboard
+    // remove_menu_page('upload.php'); // Media
+});
+
+// Remove admin toolbar menu items.
+add_action('admin_bar_menu', function (WP_Admin_Bar $menu) {
+    $menu->remove_node('comments'); // Comments
+    $menu->remove_node('customize'); // Customize
+    $menu->remove_node('dashboard'); // Dashboard
+    $menu->remove_node('edit'); // Edit
+    $menu->remove_node('menus'); // Menus
+    $menu->remove_node('new-content'); // New Content
+    $menu->remove_node('search'); // Search
+    // $menu->remove_node('site-name'); // Site Name
+    $menu->remove_node('themes'); // Themes
+    $menu->remove_node('updates'); // Updates
+    $menu->remove_node('view-site'); // Visit Site
+    $menu->remove_node('view'); // View
+    $menu->remove_node('widgets'); // Widgets
+    $menu->remove_node('wp-logo'); // WordPress Logo
+}, 999);
+
+// Remove admin dashboard widgets.
+add_action('wp_dashboard_setup', function () {
+    global $wp_meta_boxes;
+
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']); // Activity
+    // unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']); // At a Glance
+    unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_site_health']); // Site Health Status
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']); // WordPress Events and News
+    unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']); // Quick Draft
+});
