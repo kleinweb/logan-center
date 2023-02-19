78221595f4a5819933ba559d4c7e677d844714a3
Chris Montgomery <chris@cdom.io>
2023-02-19 15:24:42 -0500

    feat(gql): store endpoint url as constant for type-validation

--- apps/nextjs/lib/constants.ts
+++ apps/nextjs/lib/constants.ts
@@ -2,3 +2,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 export const SITE_NAME = 'Logan Center for Urban Investigative Reporting'
+
+/**
+ * GraphQL server endpoint for this environment.
+ *
+ * While this is provided for convenience, we also assure TypeScript that
+ * this will never be `undefined`. By default, any environment variable has the
+ * type `string | undefined`.
+ */
+export const GRAPHQL_ENDPOINT = process.env
+  .NEXT_PUBLIC_GRAPHQL_ENDPOINT as string
