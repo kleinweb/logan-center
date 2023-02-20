5c6cee8a1165403cb98e9cfedd9baa7d756ff0db
Chris Montgomery <chris@cdom.io>
2023-02-20 16:52:21 -0500

    fix(sb): load typekit and tailwind in stories

--- apps/nextjs/.storybook/main.ts
+++ apps/nextjs/.storybook/main.ts
@@ -3,7 +3,10 @@
 
 import type {StorybookConfig} from '@storybook/nextjs'
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../@(components|pages|stories)/**/*.mdx',
+    '../@(components|pages|stories)/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
--- apps/nextjs/.storybook/preview.ts
+++ apps/nextjs/.storybook/preview.ts
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2023 Temple University <kleinweb@temple.edu>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import '../styles/globals.css'
+
 export const parameters = {
   backgrounds: {
     default: 'light',
