54900285e0f0df4c5ad8b507a5a86b2927af5146
Chris Montgomery <chris@cdom.io>
2023-02-19 20:06:54 -0500

    fix(nix): standard upgrade flake-parts incompat

--- flake.nix
+++ flake.nix
@@ -34,24 +34,34 @@
 
   outputs = {
     std,
-    flake-parts,
+    self,
     ...
   } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux"];
-      perSystem = {self', ...}: {_module.args = {inherit (self') cells;};};
-      std.grow.cellsFrom = ./nix;
-      std.grow.cellBlocks = [
-        (std.blockTypes.data "meta")
-        (std.blockTypes.devshells "devshells")
-        (std.blockTypes.functions "devshellProfiles")
-        (std.blockTypes.functions "lib")
-        (std.blockTypes.nixago "nixago")
+    std.growOn {
+      inherit inputs;
+      # 1. Each folder inside `cellsFrom` becomes a "Cell"
+      #    Run for example: 'mkdir nix/mycell'
+      # 2. Each <block>.nix or <block>/default.nix within it becomes a "Cell Block"
+      #    Run for example: '$EDITOR nix/mycell/packages.nix' - see example content below
+      cellsFrom = ./nix;
+      # 3. Only blocks with these names [here: "packages" & "devshells"] are picked up by Standard
+      #    It's a bit like the output type system of your flake project (hint: CLI & TUI!!)
+      cellBlocks = with std.blockTypes; [
+        (devshells "devshells" {ci.build = true;})
+        (data "meta")
+        (functions "devshellProfiles")
+        (functions "lib")
+        (installables "packages" {ci.build = true;})
+        (nixago "nixago")
       ];
-      std.harvest = {
-        devShells = ["_automation" "devshells"];
-      };
-      imports = [inputs.std.flakeModule];
+    }
+    # 4. Run 'nix run github:divnix/std'
+    # 'growOn' ... Soil:
+    #  - here, compat for the Nix CLI
+    #  - but can use anything that produces flake outputs (e.g. flake-parts or flake-utils)
+    # 5. Run: nix run .
+    {
+      devShells = ["_automation" "devshells"];
     };
 
   nixConfig = {
