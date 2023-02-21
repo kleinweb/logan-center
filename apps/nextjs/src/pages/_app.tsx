2f98723b4cc1066ff905ecb1faf1d8e2a8b38e35
Chris Montgomery <chris@cdom.io>
2023-02-21 18:32:10 -0500

    fix: cleanup _app.tsx for fresh start and passing builds

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -2,48 +2,45 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 import type {AppProps} from 'next/app'
+import {ApolloProvider} from '@apollo/client'
 
+///: queries
+// import {SitewideDocument} from '@/gql/graphql'
+
+///: helpers
 import useInternalLinkRouting from '@/lib/hooks/useInternalLinkRouting'
+import {
+  // addApolloState,
+  // initializeApollo,
+  useApollo,
+} from '@/lib/graphql'
 
+///: global components
 import Telemetry from '@components/Telemetry'
 
-import '@/styles/globals.css'
-// import SiteSettingsProvider from '@/contexts/SiteSettings'
-// import {PageProps} from '@/lib/types'
-// import SiteMenusProvider from '@/contexts/SiteMenus'
-// import {useSiteMenusQuery} from '@/graphql/generated'
-import {ApolloProvider} from '@apollo/client'
-import {addApolloState, initializeApollo, useApollo} from '@/lib/graphql'
-import {SitewideDocument} from '@/gql/graphql'
+///: global styles
+import '@/src/styles/globals.css'
 
 export default function App({Component, pageProps}: AppProps) {
   const apolloClient = useApollo(pageProps)
-  // const {data} = pageProps as PageProps
   useInternalLinkRouting()
   return (
     <ApolloProvider client={apolloClient}>
-      {/* <SiteSettingsProvider generalSettings={data.generalSettings}> */}
-      {/* <SiteMenusProvider
-        headerMenu={data?.headerMenu}
-        // footerMenu={data?.footerMenu}
-      > */}
       <Telemetry />
       <Component {...pageProps} />
-      {/* </SiteMenusProvider> */}
-      {/* </SiteSettingsProvider> */}
     </ApolloProvider>
   )
 }
 
-export async function getStaticProps() {
-  const apolloClient = initializeApollo()
-  await apolloClient.query({
-    query: SitewideDocument,
-  })
+// export async function getStaticProps() {
+//   const apolloClient = initializeApollo()
+//   await apolloClient.query({
+//     query: SitewideDocument,
+//   })
 
-  return addApolloState(apolloClient, {
-    props: {},
-    // FIXME: this is probably a bad idea value
-    // revalidate: 1,
-  })
-}
+//   return addApolloState(apolloClient, {
+//     props: {},
+//     // FIXME: this is probably a bad idea value
+//     // revalidate: 1,
+//   })
+// }
