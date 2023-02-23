d06a5cbe5484bcce6cb1114a2965189f9b2fc23c
Chris Montgomery <chris@cdom.io>
2023-02-22 22:30:50 -0500

    chore(req): bulk delete scattered fragments and queries
    Some will probably return, but I need to clear away the clutter to retain only
    what we need.

--- apps/nextjs/lib/types.d.ts
+++ apps/nextjs/lib/types.d.ts
@@ -1,22 +1,3 @@
-import {
-  ContentNode,
-  // GeneralSettings,
-  Menu,
-  // Settings,
-} from '@/gql/graphql'
-
 export interface ChildrenProps {
   children: React.ReactNode
-}
-
-export interface PageProps {
-  data: {
-    // generalSettings: GeneralSettings
-    headerMenu: Menu
-    footerMenu?: Menu
-    page?: ContentNode
-    post?: ContentNode
-    // FIXME
-    // posts?: ArchiveFields
-  }
 }
