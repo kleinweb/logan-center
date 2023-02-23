249cda414e60a66f93a4503e78afb51d244d6c3e
Chris Montgomery <chris@cdom.io>
2023-02-22 22:39:19 -0500

    feat(tw): add union type of current aspect ratio names
    If only they could be parsed from the config without bloat...

--- apps/nextjs/lib/types.d.ts
+++ apps/nextjs/lib/types.d.ts
@@ -1,3 +1,13 @@
 export interface ChildrenProps {
   children: React.ReactNode
 }
+/**
+ * Named aspect ratios supported in our Tailwind theme.
+ *
+ * Unfortunately, it seems that parsing the fully-resolved Tailwind
+ * configuration would dramatically increase the resulting bundle size. We can
+ * export the configuration easily to a JSON file, but it turns out that casting
+ * a parsed JSON string to TypeScript types is not particularly straightforward
+ * (as far as I can tell).
+ */
+export type AspectRatioNames = 'square' | 'video' | 'banner' | 'cinematic'
