ba8778b8977ad868796208c264fa2204ef81e757
Chris Montgomery <chris@cdom.io>
2023-02-23 20:32:15 -0500

    feat: add consts for organization names

--- apps/nextjs/lib/constants.ts
+++ apps/nextjs/lib/constants.ts
@@ -1,10 +1,11 @@
 // SPDX-FileCopyrightText: 2023 Temple University <kleinweb@temple.edu>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-export const SITE_NAME = 'Logan Center for Urban Investigative Reporting'
-
+export const TU_NAME = 'Temple University'
 /** TU Cherry color in unprefixed hexadecimal format. */
 export const TU_BRAND_COLOR = 'a41e35'
+export const ORG_NAME = 'Klein College of Media and Communication'
+export const SITE_NAME = 'Logan Center for Urban Investigative Reporting'
 
 /** Base URL route for author archives and profile pages. */
 export const AUTHOR_ROUTES_BASE = '/authors'
