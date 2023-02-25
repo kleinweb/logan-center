1cd966c0ea787d557c8c24a266d1a5d0e1a1512f
Chris Montgomery <chris@cdom.io>
2023-02-24 22:46:33 -0500

    feat(layout): let Container accept classnames

--- apps/nextjs/src/templates/Container/Container.tsx
+++ apps/nextjs/src/templates/Container/Container.tsx
@@ -1,14 +1,18 @@
 // SPDX-FileCopyrightText: 2022-2023 Temple University <kleinweb@temple.edu>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import {ChildrenProps, ClassNameProps} from '@/lib/types'
+import clsx from 'clsx'
 import * as React from 'react'
 
-type Props = {
-  children?: React.ReactNode
-}
+type ContainerProps = ChildrenProps & ClassNameProps
 
-const Container = ({children}: Props) => {
-  return <div className="container relative mx-auto px-5">{children}</div>
+const Container = ({children, className}: ContainerProps) => {
+  return (
+    <div className={clsx('container relative mx-auto px-5', className)}>
+      {children}
+    </div>
+  )
 }
 
 export default Container
