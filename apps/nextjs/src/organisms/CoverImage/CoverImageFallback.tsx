596f23884103601b4977eea348eb653f42a72601
Chris Montgomery <chris@cdom.io>
2023-02-24 23:10:30 -0500

    fix: restore cover image fallback as svg not image

--- apps/nextjs/src/organisms/CoverImage/CoverImageFallback.tsx
+++ apps/nextjs/src/organisms/CoverImage/CoverImageFallback.tsx
@@ -1,13 +1,12 @@
 // SPDX-FileCopyrightText: 2023 Temple University <kleinweb@temple.edu>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import Image from 'next/image'
-import motifBackground from '@/public/assets/decorations/motif--bg.svg'
+import MotifBackground from '@/public/assets/decorations/motif--bg.svg'
 
 export default function CoverImageFallback() {
   return (
     <div className="h-28">
-      <Image src={motifBackground} alt="" fill />
+      <MotifBackground />
     </div>
   )
 }
