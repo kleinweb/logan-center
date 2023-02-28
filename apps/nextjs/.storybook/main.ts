c2b911c9e3c4915fcfd3af632e59b11b03c0706a
Chris Montgomery <chris@cdom.io>
2023-02-27 23:37:11 -0500

    chore(deps|sb): attempt to fix latest storybook again

--- apps/nextjs/.storybook/main.ts
+++ apps/nextjs/.storybook/main.ts
@@ -3,19 +3,18 @@
 
 import type {StorybookConfig} from '@storybook/nextjs'
 const config: StorybookConfig = {
-  stories: ['../src', '../stories'],
+  stories: ['../src/**/*.@(mdx|stories.@(jsx|tsx))'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-mdx-gfm',
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: true,
   },
 }
 export default config
