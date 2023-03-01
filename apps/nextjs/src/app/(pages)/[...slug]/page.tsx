15e87ae8dbe2b4c6630a7bb559590b70355f0f15
Chris Montgomery <chris@cdom.io>
2023-02-28 22:28:41 -0500

    feat(pages|urls): dynamic catchall routes for single pages

---
+++ apps/nextjs/src/app/(pages)/[...slug]/page.tsx
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: 2022-2023 Temple University <kleinweb@temple.edu>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// import {log} from '@/lib/log'
+import getApolloClient from '@/lib/graphql/client'
+import {
+  // AllPagesPathsDocument,
+  SinglePageDocument,
+  SinglePageQuery,
+} from '@/gql/graphql'
+
+import PageHeader from '@organisms/Page/PageHeader'
+import PostBody from '@organisms/PostBody'
+
+export const metadata = {
+  // FIXME: generate dynamically: <https://beta.nextjs.org/docs/api-reference/file-conventions/head#dynamic-metadata>
+  title: 'About',
+  // TODO
+  // openGraph={{imageUrl: data?.post.featuredImage?.node.sourceUrl}}
+}
+
+// FIXME: "Error: Page changed from static to dynamic at runtime"
+// export async function generateStaticParams() {
+//   const allPages = await getApolloClient().query({query: AllPagesPathsDocument})
+//   const allPagesSlugs = allPages.data.pages.edges.map(page => ({
+//     slug: page.node.uri.split('/').filter(s => s !== ''),
+//   }))
+//   log('allPages', {
+//     allPages: JSON.stringify(allPages),
+//     slugs: JSON.stringify(allPagesSlugs),
+//   })
+//   return allPagesSlugs
+// }
+
+export default async function SinglePage({params}: {params: {slug: string[]}}) {
+  const uri = '/' + params.slug.join('/')
+  const {data} = await getApolloClient().query<SinglePageQuery>({
+    query: SinglePageDocument,
+    variables: {uri},
+  })
+  const {title, content} = data.page
+  return (
+    <article>
+      <PageHeader title={title} />
+      <PostBody content={content} />
+    </article>
+  )
+}
