b818ca742a1609f4c2f76c3ff28d412bfa3fe38b
Chris Montgomery <chris@cdom.io>
2023-03-02 22:14:07 -0500

    chore(reuse): annotate

--- packages/logan-center-plugin/src/ContentTypes/Article.php
+++ packages/logan-center-plugin/src/ContentTypes/Article.php
@@ -1,5 +1,8 @@
 <?php
 
+// SPDX-FileCopyrightText: 2023 Temple University <kleinweb@temple.edu>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 namespace Klein\LoganCenter\ContentTypes;
 
 use ExtCPTs\Args\PostType as PostTypeArgs;
