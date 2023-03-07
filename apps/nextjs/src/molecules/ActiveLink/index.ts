e045bcbf8f848cda56c992df769c1ae3333b1526
Chris Montgomery <chris@cdom.io>
2023-03-06 22:46:06 -0500

    feat(queries): add assortment of fragments and queries

--- apps/nextjs/src/molecules/ActiveLink/index.ts
+++ apps/nextjs/src/molecules/ActiveLink/index.ts
@@ -1,6 +1,5 @@
 // SPDX-FileCopyrightText: 2023 Temple University <kleinweb@temple.edu>
 // SPDX-FileCopyrightText: 2023 Vercel
-// SPDX-License-Identifier: GPL-3.0-or-later
 // SPDX-License-Identifier: GPL-3.0-or-later OR MIT
 
 export {default as ActiveLink} from './ActiveLink'
